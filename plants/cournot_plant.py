b38f34781671e3947684d9a26336060c87a02f40
Jesper Elverum <jesper240800@gmail.com>
2024-01-19 13:25:36 +0100

    Changes to make cournot plant compatible

--- plants/cournot_plant.py
+++ plants/cournot_plant.py
@@ -1,5 +1,5 @@
-from plant import Plant
-import numpy as np
+from .plant import Plant
+import jax.numpy as jnp
 
 class CournotPlant(Plant):
     def __init__(self, pMax, cm):
@@ -9,19 +9,20 @@ class CournotPlant(Plant):
         self.q1 = 0.5
         self.q2 = 0.5
         self.T = 2
-    def process(self, control_signal):
+    def process(self, control_signal, noise):
 
         self.q_1 = self.q1 + control_signal
-        self.q2 = self.q2 + np.random.uniform(-0.01, 0.01)
-        self.q1 = np.clip(self.q1, 0, 1)
-        self.q2 = np.clip(self.q2, 0, 1)
+        self.q2 = self.q2 + noise
+        self.q1 = jnp.clip(self.q1, 0, 1)
+        self.q2 = jnp.clip(self.q2, 0, 1)
 
         q = self.q1 + self.q2
         price = self.pMax - q
         profit = self.q1 * (price - self.cm)
-        error  = self.T - profit
-        return error
+        return profit
     
+    def get_target(self):
+        return self.T
 if __name__ == '__main__':
     plant = CournotPlant(2, 0.1)
     print(plant.process(0.01))
