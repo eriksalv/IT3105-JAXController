f4cc9e796bc6cf3491970f2befa840398094be0d
Jesper Elverum <jesper240800@gmail.com>
2024-01-22 11:46:23 +0100

    Added plots

--- consys.py
+++ consys.py
@@ -1,10 +1,13 @@
 from controllers.controller import Controller
 from plants.plant import Plant
 from plants.bathtub_plant import BathtubPlant
+from plants.cournot_plant import CournotPlant
+
 from controllers.pid_controller import PIDController
 from controllers.neural_net_controller import NNController
 from jax import value_and_grad, random, tree_util
 import jax.numpy as jnp
+import matplotlib.pyplot as plt
 
 
 class CONSYS:
@@ -18,16 +21,17 @@ class CONSYS:
     def run_system(self):
         params = self.controller.gen_params()
         grad_func = value_and_grad(self.run_epoch, argnums=0)
-
+        all_mse = []
         for _ in range(self.epochs):
             flat_params = tree_util.tree_leaves(params)
             self.plant.reset()
             self.controller.reset()
             MSE, gradients = grad_func(flat_params)
             print(MSE)
+            all_mse.append(MSE)
             params = self.controller.update_params(
                 flat_params, self.lrate, gradients)
-
+        self.plot_mse(all_mse)
     def run_epoch(self, flat_params):
         """
         Runs a single epoch and returns MSE
@@ -50,10 +54,39 @@ class CONSYS:
         error = (self.plant.get_target() - output)
         # print(f'output = {output}, error = {error}')
         return self.controller.calculate_control_value(error)
+    
+    def plot_mse(self, all_mse):
+        plt.plot(all_mse)
+        plt.xlabel('Epoch')
+        plt.ylabel('MSE')
+        plt.title('Learning Progression')
 
-
+        plt.show()
+        return
+    
 if __name__ == '__main__':
+    print("Standard bathtub")
     plant = BathtubPlant(100, 1, 50)
+    controller = PIDController()
+    consys = CONSYS(plant, controller)
+    consys.run_system()
+    controller.plot_params()
+
+    print("Neural net bathtub")
+    plant = BathtubPlant(100, 1, 50)
+    controller = NNController()
+    consys = CONSYS(plant, controller)
+    consys.run_system()
+
+    print("Standard cournot")
+    plant = CournotPlant(5, 1)
+    controller = PIDController()
+    consys = CONSYS(plant, controller)
+    consys.run_system()
+    controller.plot_params()
+    
+    print("Neural net cournot")
+    plant = CournotPlant(5, 1)
     controller = NNController()
     consys = CONSYS(plant, controller)
     consys.run_system()
--- controllers/pid_controller.py
+++ controllers/pid_controller.py
@@ -1,7 +1,8 @@
 from .controller import Controller
 import jax.numpy as jnp
 from jax import tree_util
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 class PIDController(Controller):
     def gen_params(self):
@@ -9,7 +10,10 @@ class PIDController(Controller):
         ki = jnp.float32(2.0)
         kd = jnp.float32(3.0)
 
+        self.param_history = []
+        self.param_history.append([kp, ki, kd])
         params = (kp, ki, kd)
+ 
         self.treedef = tree_util.tree_structure(params)
         return params
 
@@ -27,12 +31,19 @@ class PIDController(Controller):
         kp_new = kp - lrate * gradients[0]
         ki_new = ki - lrate * gradients[1]
         kd_new = kd - lrate * gradients[2]
-
+        self.param_history.append([kp_new, ki_new, kd_new])
         return kp_new, ki_new, kd_new
 
-    def get_params(self):
-        return
+    def plot_params(self):
+        param_history_array = np.array(self.param_history)
 
+        plt.plot(param_history_array[:,0], label='kp')
+        plt.plot(param_history_array[:,1], label = 'ki')
+        plt.plot(param_history_array[:,2], label = 'kd')
+
+        plt.xlabel('Epoch')
+        plt.legend()
+        plt.show()
 
 if __name__ == '__main__':
     con = PIDController()
